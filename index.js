7f47f5c822ad06981e8bb8a302b6bf1eea03b93b
tahakinci <tahakinci29@gmail.com>
2023-10-20 11:22:25 +0300

    .

--- index.js
+++ index.js
@@ -26,11 +26,8 @@ const generateId = () => {
 };
 
 app.use(express.json());
+app.use(express.static("dist"));
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
-});
 
 app.get("/api/notes", (req, res) => {
   res.json(notes);
