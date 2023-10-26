00ec7caf3c2d8bf37a33b6df21bc14898228010e
tahakinci <tahakinci29@gmail.com>
2023-10-26 10:37:29 +0300

    end of part-4/2

---
+++ app.js
@@ -0,0 +1,32 @@
+const config = require("./utils/config");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const express = require("express");
+require("express-async-errors");
+const app = express();
+const cors = require("cors");
+const notesRouter = require("./controllers/notes");
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to MONGODB/NOTES");
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((err) => logger.error("error connecting to MongoDB", err.message));
+
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/notes", notesRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://tahakinci29:${password}@phonebook-db.yktj5vj.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "HTML is easy",
+  date: new Date(),
+  important: true,
+});
+
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
+/*
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})*/
